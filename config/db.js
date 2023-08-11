e2786cebecdeb965fec799480b8fef7514245eef
Diwanshu <saidiwanshu1880@gmail.com>
2023-08-11 14:18:41 +0530

    mongo

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import colors from "colors";
 
 const connectdb = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
